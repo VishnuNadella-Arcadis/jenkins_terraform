8106a579591b50a009151baa2b68ae5efc128772
Vishnu <vishnu.nadella@arcadis.com>
2024-04-12 15:45:26 +0530

    changed bucket name

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-      bucket = "terraform-remote-backend-s3"
+      bucket = "terraform-remote-backend-env-setup"
       key = "terraform.tfstate"
       region = "ap-south-1"
     }
